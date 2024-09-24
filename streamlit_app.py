d224b6819c6b271b5873df5e4c706f60fd839516
elhamod <55500282+elhamod@users.noreply.github.com>
2024-09-24 16:32:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,16 +13,18 @@ from gcsa.recurrence import Recurrence, DAILY, SU, SA
 from beautiful_date import Jan, Apr, Sept
 
 
-calendar = GoogleCalendar('mndhamod@gmail.com', credentials_path='client_secret_920720038017-aos0aqjnr59dn3fm2ccb60fvk2qicn3e.apps.googleusercontent.com.json', authentication_flow_port=8281)
-event = Event(
-    'Breakfast',
-    start=(1 / Sep / 2019)[9:00],
-    recurrence=[
-        Recurrence.rule(freq=DAILY),
-    ]
-)
+calendar = GoogleCalendar('mndhamod@gmail.com', credentials_path='client_secret_920720038017-aos0aqjnr59dn3fm2ccb60fvk2qicn3e.apps.googleusercontent.com.json', authentication_flow_port=8281, open_browser=False)
+# event = Event(
+#     'Breakfast',
+#     start=(1 / Sep / 2019)[9:00],
+#     recurrence=[
+#         Recurrence.rule(freq=DAILY),
+#     ]
+# )
 
-calendar.add_event(event)
+# calendar.add_event(event)
 
 for event in calendar:
-    print(event)
+    st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
