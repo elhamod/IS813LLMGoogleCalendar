124ed94608c54e6594bb35b3b2649f2029e5ba38
elhamod <55500282+elhamod@users.noreply.github.com>
2024-09-24 18:01:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from gcsa.recurrence import Recurrence, DAILY, SU, SA
 from beautiful_date import Jan, Apr, Sept
 
 
-calendar = GoogleCalendar('mndhamod@gmail.com', credentials_path='client_secret_959719549897-7iirmp6t4jtj9cilcrsgig03u0g8iphr.apps.googleusercontent.com.json', open_browser=False,  authentication_flow_port=8380)
+calendar = GoogleCalendar('mndhamod@gmail.com', credentials_path='client_secret_959719549897-5rocqlvu26t7gur3ods0gjdn4kagcl9g.apps.googleusercontent.com.json', open_browser=False,  authentication_flow_port=8380)
 # event = Event(
 #     'Breakfast',
 #     start=(1 / Sep / 2019)[9:00],
