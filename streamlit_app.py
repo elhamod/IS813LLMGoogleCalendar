d28dbe5de639611b0225efd5db5c8d2ebe00ee82
elhamod <55500282+elhamod@users.noreply.github.com>
2024-09-24 22:22:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,45 @@ from gcsa.recurrence import Recurrence, DAILY, SU, SA
 
 from beautiful_date import Jan, Apr, Sept
 
+from gcsa.event import Event
+from gcsa.google_calendar import GoogleCalendar
+from gcsa.recurrence import Recurrence, DAILY, SU, SA
+from google.oauth2 import service_account
+from beautiful_date import Jan, Apr, Sept
 
-calendar = GoogleCalendar('mndhamod@gmail.com', credentials_path='client_secret_959719549897-5rocqlvu26t7gur3ods0gjdn4kagcl9g.apps.googleusercontent.com.json', open_browser=False,  authentication_flow_port=8502)
-# event = Event() #
+
+credentials = service_account.Credentials.from_service_account_file(
+        "llmcalendar2-5b26aa6480be.json",
+        scopes=["https://www.googleapis.com/auth/calendar"]
+    )
+
+
+calendar = GoogleCalendar(credentials=credentials)
+# for cal in calendar.get_calendar_list():
+#   print(cal)
+# event = Event(
 #     'Breakfast',
-#     start=(1 / Sep / 2019)[9:00],
-#     recurrence=[
-#         Recurrence.rule(freq=DAILY),
-#     ]
+#     start=(25 / Sept / 2024)[9:00],
+#     end=(25 / Sept / 2024)[10:00],
 # )
 
 # calendar.add_event(event)
-
-for event in calendar:
+# c = list(calendar.get_events(calendar_id="mndhamod@gmail.com"))
+# print(c)
+for event in c:
     st.write(event)
+
+
+# calendar = GoogleCalendar('mndhamod@gmail.com', credentials_path='client_secret_959719549897-5rocqlvu26t7gur3ods0gjdn4kagcl9g.apps.googleusercontent.com.json', open_browser=False,  authentication_flow_port=8502)
+# # event = Event() #
+# #     'Breakfast',
+# #     start=(1 / Sep / 2019)[9:00],
+# #     recurrence=[
+# #         Recurrence.rule(freq=DAILY),
+# #     ]
+# # )
+
+# # calendar.add_event(event)
+
+# for event in calendar:
+#     st.write(event)
